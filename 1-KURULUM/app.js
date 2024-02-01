f9e256d87c89817c7e6a8ac585b18485ffb0a4bd
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-02-02 01:51:02 +0300

    tuple-datatype

--- 1-KURULUM/app.js
+++ 1-KURULUM/app.js
@@ -1,2 +1,21 @@
-console.log('tyasdasd');
-console.log('tyasdasd11111');
+// let age: number = 29;
+// let firstname: string = 'Resat'
+// let lastname: string = 'Camgoz'
+// let isUpdated: boolean = true;
+// lastname = 'asdasdsad'
+//! lastname = 34  dogru değil çünkü string bir değeri number atayamazsın.
+// function display(id: any, name: string) {
+//     console.log("ıd = " + id + ", Name = " + name);
+// }
+//! DATA TYPE
+// let first:number = 123 //number
+// let second: number = 0x37CF // hexadecimal
+// let third:number = 0o377 // octal
+// let fourth: number = 0b111001 //binary
+// let employeefirstName: string = 'reso'
+// console.log(employeefirstName);
+// let employeeName: string = "John"
+// let employeeDept: string = 'Finance'
+// let employeeDesc1: string = employeeName + "works in the " + employeeDept + "department"
+// let employeeDesc2: string = `${employeeName} works in the ${employeeDept} department`
+// console.log(employeeDesc1, employeeDesc2);
--- 1-KURULUM/app.ts
+++ 1-KURULUM/app.ts
@@ -1,2 +1,98 @@
-console.log('tyasdasd')
-console.log('tyasdasd11111')
+// let age: number = 29;
+// let firstname: string = 'Resat'
+// let lastname: string = 'Camgoz'
+// let isUpdated: boolean = true;
+
+// lastname = 'asdasdsad'
+//! lastname = 34  dogru değil çünkü string bir değeri number atayamazsın.
+
+// function display(id: any, name: string) {
+
+//     console.log("ıd = " + id + ", Name = " + name);
+
+// }
+
+//! DATA TYPE
+// let first:number = 123 //number
+// let second: number = 0x37CF // hexadecimal
+// let third:number = 0o377 // octal
+// let fourth: number = 0b111001 //binary
+
+// let employeefirstName: string = 'reso'
+// console.log(employeefirstName);
+
+// let employeeName: string = "John"
+// let employeeDept: string = 'Finance'
+
+// let employeeDesc1: string = employeeName + "works in the " + employeeDept + "department"
+
+// let employeeDesc2: string = `${employeeName} works in the ${employeeDept} department`
+// console.log(employeeDesc1, employeeDesc2);
+
+// let isPresent: boolean = false
+// console.log(isPresent);
+//! ARRAYS
+// let firstNames: string[] = ['Reso', 'Mehmet', 'Ahmet', 'Sinem', 'Merve']
+// let firstNames: Array<string> = ['Reso', 'Mehmet', 'Ahmet', 'Sinem', 'Merve']
+
+// let arr = [1, 3, 'Apple', 'Orange', 'Banana', true, false]
+// let fruits: Array<string>
+// fruits = ['Apple', 'Orange', 'Banana']
+
+// let ids: Array<number>
+// ids = [23,45,64,76,87]
+// let values: (string | number)[] =['Apple', 'Orange',3,5, 'Banana']
+
+
+
+// let fruits: Array<string>
+// fruits = ['Apple', 'Orange', 'Banana']
+
+// console.log(fruits[1]);
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+//! Tuple
+// let Id: number = 1
+// let instructorName: string = 'reso'
+
+// let instructor: [number, string] = [1, 'Reso']
+
+
+// var user: [number, string, boolean, number, string];
+
+// user = [1, 'Steve', true, 5, 'Reso']
+
+// console.log(user);
+
+// let employee: [number, string][];
+// employee = [[1, 'steve'], [2, 'bill'], [3, 'jeff']]
+//! Object Data-Type
+
+let
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
